97ee893ed1e99e88d0a08068c32d29f21a68bd6f
wfjackson3 <willis@mashtunbeer.com>
2010-08-16 20:15:06 -0500

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -42,8 +42,9 @@ ActionController::Routing::Routes.draw do |map|
 	map.contact	'/contact',	:controller => 'pages',	:action => 'contact'
 	map.about		'/about',		:controller => 'pages',	:action => 'about'
 	map.help		'/help',		:controller => 'pages', :action => 'help'
-	map.root								:controller => 'pages', :action => 'home'
+	map.root		:controller => 'pages', :action => 'home'
+	map.signup '/signup',		:controller => 'users',	:action => 'new'
 
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+#  map.connect ':controller/:action/:id'
+#  map.connect ':controller/:action/:id.:format'
 end
