0cb5025526d14ae967e809a9c299cbe4c5973de9
wfjackson3 <willis@mashtunbeer.com>
2010-08-31 19:59:32 -0500

    Finished first cut of the User model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,9 @@ module ApplicationHelper
 			"#{base_title} | #{@title}"
 		end
 	end
+
+# Make the logo link to the home page
+	def logo
+		image_tag "logo.png", :alt => "Sample App", :class => "round"
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -47,4 +47,6 @@ ActionController::Routing::Routes.draw do |map|
 
 #  map.connect ':controller/:action/:id'
 #  map.connect ':controller/:action/:id.:format'
+	map.resources :users
+
 end
